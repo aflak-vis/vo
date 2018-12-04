a1bbb23870e234ebde6496773a18eef23bb7918f
Malik Olivier Boussejra <malik@boussejra.com>
2018-12-04 15:22:10 +0900

    WIP

--- vo-sia/src/lib.rs
+++ vo-sia/src/lib.rs
@@ -1,3 +1,6 @@
+extern crate hyper;
+extern crate url;
+
 #[derive(Debug)]
 pub struct SiaService<'a> {
     url: &'a str,
@@ -8,28 +11,31 @@ impl<'a> SiaService<'a> {
         url: "http://dc.zah.uni-heidelberg.de/hppunion/q/im/siap.xml",
     };
 
-    pub fn create_query(&self, pos: (f32, f32)) -> SiaQuery<'a> {
+    pub fn create_query<'k>(&self, pos: (f32, f32)) -> SiaQuery<'a, 'k> {
         SiaQuery {
-            url: self.url,
+            base_url: self.url,
             pos,
             size: (1.0, 1.0),
             format: Format::All,
+            intersect: Intersect::Overlaps,
+            verbosity: Verbosity::VV,
+            keywords: vec![],
         }
     }
 }
 
-#[derive(Debug)]
-pub struct SiaQuery<'a> {
-    url: &'a str,
+#[derive(Debug, Clone, PartialEq)]
+pub struct SiaQuery<'a, 'k> {
+    base_url: &'a str,
     pos: (f32, f32),
     size: (f32, f32),
     format: Format,
-    // intersect
-    // verbosity
-    // other keywords
+    intersect: Intersect,
+    verbosity: Verbosity,
+    keywords: Vec<(&'k str, &'k str)>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum Format {
     All,
     Graphic,
@@ -39,8 +45,83 @@ pub enum Format {
     Png,
 }
 
-impl<'a> SiaQuery<'a> {
+impl From<Format> for &'static str {
+    fn from(format: Format) -> &'static str {
+        match format {
+            Format::All => "ALL",
+            Format::Graphic => "GRAPHIC",
+            Format::Metadata => "METADATA",
+            Format::Fits => "APPLICATION/FITS",
+            Format::Jpeg => "IMAGE/JPEG",
+            Format::Png => "IMAGE/PNG",
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum Intersect {
+    Covers,
+    Enclosed,
+    Overlaps,
+    Center,
+}
+
+impl From<Intersect> for &'static str {
+    fn from(intersect: Intersect) -> &'static str {
+        match intersect {
+            Intersect::Covers => "COVERS",
+            Intersect::Enclosed => "ENCLOSED",
+            Intersect::Overlaps => "OVERLAPS",
+            Intersect::Center => "CENTER",
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum Verbosity {
+    Zero = 0,
+    V = 1,
+    VV = 2,
+    VVV = 3,
+}
+
+impl<'a, 'k> SiaQuery<'a, 'k> {
     pub fn execute(&self) {
-        ()
+        use hyper::rt::{self, Future, Stream};
+        use hyper::Client;
+        use std::io::{self, Write};
+        rt::run({
+            let client = Client::new();
+            let uri = self.query_url().parse().unwrap();
+            client
+                .get(uri)
+                .and_then(|res| {
+                    println!("Response: {}", res.status());
+                    res.into_body()
+                        // Body is a stream, so as each chunk arrives...
+                        .for_each(|chunk| {
+                            io::stdout()
+                                .write_all(&chunk)
+                                .map_err(|e| panic!("example expects stdout is open, error={}", e))
+                        })
+                }).map_err(|err| {
+                    println!("Error: {}", err);
+                })
+        });
+    }
+
+    fn query_url(&self) -> String {
+        let pos_val = format!("{},{}", self.pos.0, self.pos.1);
+        let size_val = format!("{},{}", self.size.0, self.size.1);
+        let verb_val = format!("{}", self.verbosity as usize);
+        let query_string = url::form_urlencoded::Serializer::new(String::new())
+            .append_pair("POS", &pos_val)
+            .append_pair("SIZE", &size_val)
+            .append_pair("FORMAT", self.format.into())
+            .append_pair("INTERSECT", self.intersect.into())
+            .append_pair("VERB", &verb_val)
+            .extend_pairs(&self.keywords)
+            .finish();
+        format!("{}?{}", self.base_url, query_string)
     }
 }
